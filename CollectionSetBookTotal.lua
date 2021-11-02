5d586fd2aae70f3eb3c87333dd29f2cf2aac4405
Tomi H <tomi.pj.hamari@student.jyu.fi>
2021-11-02 02:33:09 +0200

    Updated to latest patch
    Removed custom progress bar and added percentages to new bars on summary page

--- CollectionSetBookTotal.lua
+++ CollectionSetBookTotal.lua
@@ -3,96 +3,34 @@ CollectionSetBookTotal = {}
 local ADDON_NAME = "CollectionSetBookTotal"
 local ADDON_DISPLAY_NAME = "Item set total progress"
 
-local UnlockedPiecesTable = {}
-
--- Used only for debugging
--- local tempAllItems, tempFoundItems
-
-local csbtTlw, csbtTexture, csbtLabel
-csbtTlw = WINDOW_MANAGER:CreateTopLevelWindow()
-csbtTlw:SetDimensions(200,200)
-csbtTlw:SetAnchor(CENTER, ZO_CollectionsBook_TopLevel, CENTER, 0, 0)
-csbtTlw:SetHidden(true)
-csbtTexture = WINDOW_MANAGER:CreateControl(nil, csbtTlw, CT_STATUSBAR)
-csbtTexture:SetAnchor(CENTER,csbtTlw,TOPLEFT,0,-265)
-ApplyTemplateToControl(csbtTexture,"ZO_ArrowProgressBarWithBG")
-csbtLabel = csbtTexture:GetNamedChild("Progress")
-ZO_StatusBar_SetGradientColor(csbtTexture, ZO_XP_BAR_GRADIENT_COLORS)
-
-
-
-csbtTexture:SetHandler("OnValueChanged", function(value)
-	-- These do nothing for the time being, they just seem to be required by SetValue and SetMinMax 
-end)
-
-
-
-csbtTexture:SetHandler("OnMinMaxValueChanged", function (_, newMin, newMax)
-	-- These do nothing for the time being, they just seem to be required by SetValue and SetMinMax
-end)
-
-
----- Provided by code65536
-local function GetCollectionStatus( )
-	local collected = 0
-	local total = 0
-	local setId = GetNextItemSetCollectionId()
-	while (setId) do
-		local setSize = GetNumItemSetCollectionPieces(setId)
-		if (setSize > 0) then
-			collected = collected + GetNumItemSetCollectionSlotsUnlocked(setId)
-			total = total + setSize
-		end
-		setId = GetNextItemSetCollectionId(setId)
-	end
-
-	return collected, total
-end
-----
-
-local function RefreshFoundItems (setItemId, slotsJustUnlockedMask)
-	local foundItems, allItems = GetCollectionStatus()
-
-	csbtTexture:SetMinMax(0, allItems)
-	csbtTexture:SetValue(foundItems)
-	csbtLabel:SetText(zo_strformat(SI_ITEM_SETS_BOOK_CATEGORY_PROGRESS, foundItems, allItems) .. "  (" .. tostring(math.floor((foundItems / allItems)*100)) .. "%)")
-	
-	EVENT_MANAGER:UnregisterForEvent(ADDON_NAME, EVENT_PLAYER_ACTIVATED)
-end
-EVENT_MANAGER:RegisterForEvent(ADDON_NAME, EVENT_PLAYER_ACTIVATED,	RefreshFoundItems)
-EVENT_MANAGER:RegisterForEvent(ADDON_NAME, EVENT_ITEM_SET_COLLECTION_UPDATED,	RefreshFoundItems)
-
-
-
-local function InitializeSetBookProgress()
-	
-	local fragment = ZO_HUDFadeSceneFragment:New(csbtTlw, 0, 0)
-	
-	--add my fragment to scenes:
-	SCENE_MANAGER:GetScene("itemSetsBook"):AddFragment(fragment)
-end
-
-
 
 local function OnAddOnLoaded(_, addOnName)
 	if addOnName == ADDON_NAME then
 		EVENT_MANAGER:UnregisterForEvent(ADDON_NAME, EVENT_ADD_ON_LOADED)
 
-		-- Crude debugging commands
-		-- SLASH_COMMANDS["/debugcollectionpostfound"] = function () 
-		-- 																	d("Found " .. tostring(tempFoundItems) .. " of " .. tostring(tempAllItems))
-		-- 																end
-		-- SLASH_COMMANDS["/debugcollectionsetmax"] = function (newMaximum)
-		-- 																		csbtTexture:SetMinMax(0, newMaximum)
-		-- 																		csbtLabel:SetText(zo_strformat(SI_ITEM_SETS_BOOK_CATEGORY_PROGRESS, tempFoundItems, newMaximum) .. "  (" .. tostring(math.floor((tempFoundItems / newMaximum)*100)) .. "%)")
-		-- 																		d("Max changed to " .. tostring(newMaximum))
-		-- 																end
-		-- SLASH_COMMANDS["/debugcollectionsetbarvalue"] = function (newValue)
-		-- 																		d("Bar value set")
-		-- 																		csbtTexture:SetValue(newValue)
-		-- 																end
-		
-		InitializeSetBookProgress()
+		ITEM_SETS_BOOK_FRAGMENT:RegisterCallback("StateChange", function(oldState, newState)
+			if newState == "showing" then
+
+				function addPercentagesToChildProgressBars(control, startNum, endNum)
+					local numChildren = zo_min(control:GetNumChildren(), endNum)
+					local numStart = zo_min(startNum, numChildren)
+						
+					for i = numStart, numChildren do
+						local child = control:GetChild(i)
+						if child and child.GetName and child.GetText and zo_plainstrfind(child:GetName():lower(), "progress") then
+							local found  = { zo_strsplit( " / ", child:GetText() ) }
+							local first = found[1]:gsub("%,","")
+							local second = found[2]:gsub("%,", "")
+							child:SetText(found[1] .. "/" .. found[2] .. " (" .. tostring(math.floor((tonumber(first) / tonumber(second))*100)) .. "%)")
+						end
+						if child then
+							addPercentagesToChildProgressBars(child, 1, 100)
+						end
+					end
+				end
+				addPercentagesToChildProgressBars(ZO_ItemSetsBook_Keyboard_TopLevelSummaryContentScrollChild, 1, 100)
+			end
+		end)
 	end
 end
 EVENT_MANAGER:RegisterForEvent(ADDON_NAME, EVENT_ADD_ON_LOADED, OnAddOnLoaded)
