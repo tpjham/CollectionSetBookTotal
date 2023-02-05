121d965521fab8808d00ab19bc6d701d9ec978b1
Tomi H <tomi.pj.hamari@student.jyu.fi>
2023-02-05 20:26:15 +0200

    Update ItemSetsSummaryPagePercentages.lua
    Add check for string length to prevent errors on transmute station

--- ItemSetsSummaryPagePercentages.lua
+++ ItemSetsSummaryPagePercentages.lua
@@ -18,10 +18,12 @@ local function OnAddOnLoaded(_, addOnName)
 					for i = numStart, numChildren do
 						local child = control:GetChild(i)
 						if child and child.GetName and child.GetText and zo_plainstrfind(child:GetName():lower(), "progress") then
-							local found  = { zo_strsplit( " / ", child:GetText() ) }
-							local first = found[1]:gsub("%,","")
-							local second = found[2]:gsub("%,", "")
-							child:SetText(found[1] .. "/" .. found[2] .. " (" .. tostring(math.floor((tonumber(first) / tonumber(second))*100)) .. "%)")
+							if string.len(child:GetText()) > 0 then
+								local found  = { zo_strsplit( " / ", child:GetText() ) }
+								local first = found[1]:gsub("%,","")
+								local second = found[2]:gsub("%,", "")
+								child:SetText(found[1] .. "/" .. found[2] .. " (" .. tostring(math.floor((tonumber(first) / tonumber(second))*100)) .. "%)")
+							end
 						end
 						if child then
 							addPercentagesToChildProgressBars(child, 1, 100)
